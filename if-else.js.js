1e3a781984c9f1269ff8fb039707e436676cfb88
deneesan21 <deneesan2108@gmail.com>
2024-11-20 07:21:10 -0800

    i implement js-if-else

---
+++ if-else.js.js
@@ -0,0 +1,25 @@
+let Marks=80;
+let Results;
+
+if(Marks>=85){
+    Results="A";
+}
+else if(Marks>=75){
+    Results="B";
+}
+else if(Marks>=65){
+    Results="D";
+}
+else if(Marks>=55){
+    Results="E";
+}
+else {
+    Results="F";
+}
+console.log(Results);
+
+
+let age=27;
+let num=age>=20? "True" : "True No" ;
+console.log(age);
+console.log(num);
